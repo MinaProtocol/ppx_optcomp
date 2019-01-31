34a07a6fc4a9e16ad197817691179f5135f46ccc
Nathan Holland <nholland94@gmail.com>
2019-01-30 17:39:27 -0800

    Add test for conditional injection

--- test/injection.ml
+++ test/injection.ml
@@ -5,14 +5,22 @@
 [%%inject "z", "123"]
 [%%inject "b", false]
 
+[%%if x = "apples"]
+[%%inject "k", k]
+[%%else]
+let k = "apples"
+[%%endif]
+
 let%expect_test "injection" =
   print_endline x;
   print_endline (string_of_int y);
   print_endline z;
   if b then print_endline "y" else print_endline "n";
+  print_endline k;
   [%expect{|
 testing 1 2 3
 123
 123
 n
+apples
   |}]
